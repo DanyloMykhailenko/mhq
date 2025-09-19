ae2beb666e52f917e468fe5ae63458476e0f29a3
DanyloMykhailenko <daniiletoia@gmail.com>
2025-09-19 16:19:57 +0400

    initialized project

---
+++ src/main/java/mhq/MhqApplication.java
@@ -0,0 +1,13 @@
+package mhq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mhq/MhqApplicationTests.java
@@ -0,0 +1,13 @@
+package mhq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
